29509d5bbed71446ba032131b6e44c1f53d77aa8
svitlyi <svitlyi_a@itstep.academy>
2023-11-26 11:09:45 +0200

    Додано перевірку віку

--- Lesson_1/main.py
+++ Lesson_1/main.py
@@ -4,6 +4,12 @@ print('Ви ввели імя ' + name)
 print('Ви ввели імя ', name)
 print(f'Ви ввели імя {name}')
 
+age = int(input('Введіть свій вік:'))
+if age < 18:
+  print('Ви неповнолітній')
+else:
+  print('Ви повнолітній')
+
 country = input('З якої ти країни: ')
 if country == 'Україна':
   print('Привіт, земляче!')
