aa14904fae220205e3341e578c16eb7c0a567183
DarkValkier <darkvalkier@gmail.com>
2023-12-03 11:41:54 +0200

    Створено заготівлю гри-файтингу

---
+++ Lesson_2/character.py
@@ -0,0 +1,21 @@
+class Character:
+    def __init__(self, name, health=100,
+                 damage=3, defence=0):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.defence = defence
+
+    def __str__(self):
+        return f'Ім\'я: {self.name}\n' \
+               f'Здоров\'я: {self.health}\n' \
+               f'Шкода: {self.damage}\n' \
+               f'Захист: {self.defence}'
+
+    def take_damage(self, damage):
+        damage = max(damage, 0)
+        self.health -= damage
+        return damage
+
+    def attack(self, target):
+        return target.take_damage(self.damage)
--- Lesson_2/main.py
+++ Lesson_2/main.py
@@ -1,1 +1,15 @@
-print('Hello world!')
+from character import Character
+
+player1 = Character('Vasya')
+player2 = Character('Petya', damage=5)
+print(player1, end='\n\n')
+print(player2)
+print()
+
+p1_damage = player1.attack(player2)
+print(f'{player1.name} атакував '
+      f'{player2.name} і наніс '
+      f'{p1_damage} шкоди')
+print()
+print(player1, end='\n\n')
+print(player2)
